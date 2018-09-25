399a1cfaf11c72ab719df79403ae8c322ae14820
Habby Whyte <hwhyte@dc0cwg8wl-m.ad.npr.org>
2018-09-25 10:19:53 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+//load expess
+const express = require('express');
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+//make new express app- call express as a function
+var app = express();
+
+//configure express static middleware
+app.use(express.static(publicPath));
+
+//set up HTTP route handlers
+//first register handler; allow us to set
+//up handler for all http get request (json data, html page)
+app.get('/', (req, res) => {
+  res.send()
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
