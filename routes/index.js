c6b93f80135f9c37f412a1509e9f166ef243003f
HWhyte <hwhyte@npr.org>
2018-10-23 14:06:18 -0400

    moved all  variables out of index.js into storyModel.js

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,6 @@ exports.singleStoryBlock = (req, res) => {
   const story= new storyModel(singleStoryDataBlock);
 
   res.render('../app/views/singleStoryBlock', {
-    singleStoryDataBlock: singleStoryDataBlock,
-    title: story.getTitle(),
-    fullPubDate: story.getFullPubDate(),
-    pubDateTime: story.getPubDateTime(),
-    author: story.getAuthor(),
-    slug: story.getSlug(),
-    blockQuote: story.getBlockQuote(),
-
+    story:story,
   });
 };
